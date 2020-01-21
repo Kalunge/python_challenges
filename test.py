7be2bd316443f7d383b463a4efb049751d417fb1
Kalungez <muthomititus+github@gmail.com>
2020-01-21 18:41:46 +0300

    add guessing game

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 # SIM CARD
 print('WELCOME TO SAFARICOM. KINDLY UNLOCK YOUR SIMCARD TO PROCEED')
-attempts = 2
+attempts = 1
 pin = 4040
 
-while attempts < 3:
+while attempts <= 3:
     entered_pin = int(input(f'Enter you sim pin. you have {attempts} remaining: '))
     if entered_pin != pin:
-        attempts += 1
         remaining_attempts = 3 - attempts 
+        attempts += 1
         print(f'Wrong pin you have {remaining_attempts} attempts remaining')
         if remaining_attempts == 0:
             print('SIMCARD BLOCKED')
